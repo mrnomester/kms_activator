4a37cb91ee8b762abf8a89c4f13067a5bf133a8e
mrnomester <146173351+mrnomester@users.noreply.github.com>
2025-07-22 12:19:19 +0500

    Add files via upload

---
+++ kms.ps1
@@ -0,0 +1,20 @@
+﻿# Функция для проверки прав администратора
+function Test-IsAdmin {
+    $identity = [System.Security.Principal.WindowsIdentity]::GetCurrent()
+    $principal = New-Object System.Security.Principal.WindowsPrincipal($identity)
+    return $principal.IsInRole([System.Security.Principal.WindowsBuiltInRole]::Administrator)
+}
+
+# Проверяем, запущен ли скрипт с правами администратора
+if (-not (Test-IsAdmin)) {
+    Write-Host "Скрипт не запущен с правами администратора. Перезапускаем с повышенными правами..."
+    
+    # Получаем полный путь к текущему скрипту
+    $scriptPath = $MyInvocation.MyCommand.Definition
+    
+    # Запускаем скрипт с повышенными правами
+    Start-Process -FilePath "powershell.exe" -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File `"$scriptPath`"" -Verb RunAs
+    exit
+}
+
+irm https://get.activated.win | iex
